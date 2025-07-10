1b7f635c4a25a0ed83332b7a814ee5adc5baa901
Chia-L <lijia@clerware.com>
2025-07-10 20:04:12 +0800

    添加 扫描文本并包裹词条工具

--- src/i18n/lang/en.js
+++
@@ -1,4 +0,0 @@
-export const _langName = 'English(US)'
-export default {
-  '标签管理todo': 'Tag Management Todo'
-}
--- src/i18n/lang/zh-CN.js
+++
@@ -1,4 +0,0 @@
-export const _langName = '中文'
-export default {
-  '标签管理todo': '标签管理todo'
-}
